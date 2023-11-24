0cba37a4d4611411af61a02ef599f19bae680f47
Oriel Cedeño <65096837+Dex97r@users.noreply.github.com>
2023-11-24 11:40:26 -0500

    lab14
    lab14

---
+++ lab14/lab142.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+?>
+<?php
+if (isset($_SESSION["usuario_valido"]))
+{
+<p>[<A HREF='login.php'>Menú Principal</A>]</p>
+
+}
+else{
+print("<BR><BR>\n");
+print("<P ALIGN='CENTER'>Acceso no autorizado</P>\n");
+print("P ALIGN='CENTER'>[<A HREF='login.php' TARGET='_top'>Conectar</A>]</P>\n");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 9.1</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <h1>Consulta de noticias</h1>
+    <?php 
+        require_once("class/noticias.php");
+    
+        $obj_noticia=new noticia();
+        $noticias = $obj_noticia->consultar_noticias();
+        $nfilas = count($noticias);
+        if($nfilas > 0){
+            print ("<table\n>");
+            print ("<tr\n>");
+            print ("<th>Titutlo</th>\n");
+            print ("<th>Texto</th>\n");
+            print ("<th>Categoria</th>\n");
+            print ("<th>Fecha</th>\n");
+            print ("<th>Imagen</th>\n");
+            print ("</tr>\n");
+            
+            foreach($noticias as $resultado){
+                print("<tr>\n");
+                print("<td>" . $resultado['titulo'] ."</td>\n");
+                print("<td>" . $resultado['texto'] ."</td>\n");
+                print("<td>" . $resultado['categoria'] ."</td>\n");
+                print("<td>" . date("j/n/Y",strtotime($resultado['fecha']))."</td>\n");
+            
+
+                if($resultado['imagen'] != ""){
+                    print ("<td><a target='_blank' href='img/'>" . $resultado['imagen'] . "'><img border='0' src='img/iconotexto.gif'></a></td>\n");
+                }
+                else{
+                    print ("<td>&nbsp;</td>\n");
+                }
+                print ("</tr>\n");
+            }
+            print ("</table>\n");
+        }
+        else{
+            print ("No hay noticias disponibles");
+        }
+    ?>
+</body>
+</html>
